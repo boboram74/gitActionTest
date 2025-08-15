e895a27d821e54b8d01d7d512f46320a8656017b
qhfka74 <shj04367@gmail.com>
2025-08-15 17:40:37 +0900

    qhfka74/커밋테스트_v12

--- src/main/java/core/ghayoun/mygitai/git/service/CommitServiceImpl.java
+++ src/main/java/core/ghayoun/mygitai/git/service/CommitServiceImpl.java
@@ -32,20 +32,19 @@ public class CommitServiceImpl implements CommitService {
     @Value("${llm.api.model}")
     private String model;
 
+    @Value("${llm.api.prompt}")
+    private String prompt;
+
     @Override
     public ResponseEntity<String> getMessage(String data) throws Exception{
         System.out.println("실행횟수 = " + count.incrementAndGet());
         long startTime = System.currentTimeMillis();
         List<Message> messages = Arrays.asList(
-                new Message("system", "당신은 커밋내용 요약 전문가입니다. 규칙은 -는 삭제한 코드 +는 추가한 코드입니다.\n" +
-                        "답변은 항상 일관되게 아래 형식을 따르세요.\n" +
-                        "1. 커밋한사람/커밋명\n" +
-                        "2. 기존파일명과 기존 코드\n" +
-                        "3. 변경파일명과 변경 코드\n" +
-                        "4. 어떤 기능이 변경되었는지 간단하게 요약"),
+                new Message("system", prompt),
                 new Message("user", data)
         );
-        Options options = new Options(16, 0.2, 2048, 2048);
+        System.out.println("요청한 프롬프트 = " + prompt);
+        Options options = new Options(16, 0.0, 2048, 2048);
         OllamaRequest requestPayload = new OllamaRequest(model, messages, false, options);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
