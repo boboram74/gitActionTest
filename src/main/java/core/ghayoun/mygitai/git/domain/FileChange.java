e22016728841d45c5839f4bae257b1a8898f7689
qhfka74 <shj04367@gmail.com>
2025-08-15 16:03:01 +0900

    qhfka74/커밋테스트_v5

--- src/main/java/core/ghayoun/mygitai/git/domain/FileChange.java
+++ src/main/java/core/ghayoun/mygitai/git/domain/FileChange.java
@@ -1,10 +1,13 @@
 package core.ghayoun.mygitai.git.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FileChange {
     private String filename;
+    private String previousFilename;
     private String status;
     private Integer additions;
     private Integer deletions;
